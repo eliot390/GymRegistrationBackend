c4d25cb7e9fbe49baa578d5c0daebcaf3a40253f
eliot390 <eliot390@github.com>
2024-04-29 10:37:34 -0700

    Added methods for deleting, posting & getting single user

--- GymRegistrationServer/src/main/java/com/example/GymRegistrationServer/member/MemberController.java
+++ GymRegistrationServer/src/main/java/com/example/GymRegistrationServer/member/MemberController.java
@@ -1,6 +1,7 @@
 package com.example.GymRegistrationServer.member;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -23,13 +24,51 @@ public class MemberController {
     return memberService.getMembers();
   }
 
+  @Autowired
+  private MemberRepository memberRepository;
+  @GetMapping("/{id}")
+  /**
+   * Get single user
+   * http://localhost:8080/api/v1/member/4
+   * @param id
+   */
+  public ResponseEntity<Member> getMemberById(@PathVariable Long id){
+    return memberRepository.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+  }
+
   @PostMapping
+  /**
+   * Add new member
+   * @param member
+   * {
+   *     "name": "Cheddar",
+   *     "email": "cheds@gmail.com",
+   *     "birthday": "2022-07-26"
+   * }
+   */
   public void registerNewMember(@RequestBody Member member){
     memberService.addNewMember(member);
   }
 
   @DeleteMapping(path = "{memberId}")
+  /**
+   * http://localhost:8080/api/v1/member/4
+   * @param id
+   */
   public void deleteMember(@PathVariable("memberId") Long id){
     memberService.deleteMember(id);
   }
+
+  @PutMapping(path = "{memberId}")
+  /**
+   * Update/change member info
+   * http://localhost:8080/api/v1/member/4?name=Name&email=Email
+   * @param id
+   * @param name
+   * @param email
+   */
+  public void updateMember(@PathVariable("memberId") Long id,
+                           @RequestParam(required = false) String name,
+                           @RequestParam(required = false) String email) {
+    memberService.updateMember(id, name, email); }
 }
--- GymRegistrationServer/src/main/java/com/example/GymRegistrationServer/member/MemberService.java
+++ GymRegistrationServer/src/main/java/com/example/GymRegistrationServer/member/MemberService.java
@@ -2,8 +2,10 @@ package com.example.GymRegistrationServer.member;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -34,5 +36,20 @@ public class MemberService {
       throw new IllegalStateException("Member ID '" + id + "' does not exist.");
     }
     memberRepository.deleteById(id);
+  }
+
+  @Transactional
+  public void updateMember(Long id, String name, String email) {
+    Member member = memberRepository.findById(id).orElseThrow(() -> new IllegalStateException("Member does not exist."));
+    if (name != null && !name.isEmpty() && !Objects.equals(member.getName(), name)){
+      member.setName(name);
+    }
+    if (email != null && !email.isEmpty() && !Objects.equals(member.getEmail(), email)){
+      Optional<Member> memberOptional = memberRepository.findMemberByEmail(email);
+      if (memberOptional.isPresent()){
+        throw new IllegalStateException("Email already in use.");
+      }
+      member.setEmail(email);
+    }
   }
 }
